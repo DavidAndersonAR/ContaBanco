d5c3d67b3e1ede1d65cba96430aae7e9cce9e37c
David Anderson <davidanderson.ads2020@gmail.com>
2024-04-27 17:39:20 -0300

    simulado conta bancaria

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+
+       // TODO: Conhecer e importar a classe Scanner
+       System.out.println("DIgite numero do cliente: ");
+       int numero = sc.nextInt();
+
+       System.out.println("DIgite numero da agencia: ");
+       String agencia = sc.next();
+
+       System.out.println("DIgite o nome do cliente: ");
+       String nomeCliente = sc.next();
+
+       System.out.println("DIgite o saldo da conta: ");
+       double saldo = sc.nextFloat();
+
+       System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo +" já está disponível para saque");
+       
+
+       // TODO: Exibir as mensagens para o nosso usuario
+
+       // TODO: Obter pela scanner os valores digitados no terminal
+
+       // TODO: Exibir a mensagem: conta criada com sucesso!!!
     }
+
+    
 }
